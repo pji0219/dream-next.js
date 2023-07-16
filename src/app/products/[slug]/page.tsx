4f4cd9687ebb57c5f39b323c263edf9b1d99fc12
pji0219 <pji219@naver.com>
2023-07-16 21:23:43 +0900

    generateStaticParms

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -7,3 +7,16 @@ type Props = {
 export default function Pants({ params }: Props) {
   return <h1>{params.slug} 제품 설명 페이지!</h1>;
 }
+
+/* 
+  원래 동적 라우팅에 사용되는 페이지 골격은 정적으로 프리 렌더링 해서 만들어져 있고 slug를 통해서 들어온 키워드를 리액트 props로
+  받아와서 동적 라우팅이 되는데 
+  generateStaticParams를 사용 해서 원하는 키워드에 한해서는 미리 정적으로 프리 렌더링 해둘 수 있다.
+*/
+export function generateStaticParams() {
+  const products = ['pants', 'skirt'];
+
+  return products.map((product) => ({
+    slug: product,
+  }));
+}
