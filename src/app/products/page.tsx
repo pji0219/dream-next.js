10006f50ff62c045e94a71f8914c16915b4c166f
pji0219 <pji219@naver.com>
2023-12-03 00:00:18 +0900

    link 수정

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,17 +1,16 @@
 import Link from 'next/link';
-import styles from './page.module.css';
+
+const products = ['shirt', 'pants', 'skirt', 'shoes'];
 
 export default function Products() {
-  const products = ['shirt', 'pants', 'skirt', 'shoes'];
-
   return (
     <>
       <h1>제품 소개 페이지!</h1>
-      <ul className={styles.ul}>
+      <ul>
         {products.map((product, index) => (
-          <Link href={`/products/${product}`} key={index}>
-            {product}
-          </Link>
+          <li key={index}>
+            <Link href={`/products/${product}`}>{product}</Link>
+          </li>
         ))}
       </ul>
     </>
