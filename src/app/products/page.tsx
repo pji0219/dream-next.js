4d31cb85fe7f8392017b6317f8f06d603ed8e578
pji0219 <pji219@naver.com>
2024-01-11 23:04:21 +0900

    fetch CSR

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,6 +1,6 @@
-import { getProducts } from '@/service/products';
 import Link from 'next/link';
-import styles from './page.module.css';
+import { getProducts } from '@/service/products';
+import MeowArticle from '@/components/MeowArticle';
 
 // ISR을 하고 싶으면 page나 layout파일에 써주면 됨, 3초마다 ISR
 // export const revalidate = 3;
@@ -12,11 +12,11 @@ export default async function ProductsPage() {
 
   // 네트워크 통신으로 데이터베이스에 있는 데이터를 가져오기 위해서 서버 컴포넌트에서도 fetch사용 가능
   // 두번째 인자에 저렇게 적어주면 3초마다 fetch해주게 된다 즉 3초마다 ISR로 렌더링된다. 0으로 할 경우에는 SSR이 된다.
-  const res = await fetch('https://meowfacts.herokuapp.com', {
-    next: { revalidate: 3 },
-  });
-  const data = await res.json();
-  const factText = data.data[0];
+  // const res = await fetch('https://meowfacts.herokuapp.com', {
+  //   next: { revalidate: 3 },
+  // });
+  // const data = await res.json();
+  // const factText = data.data[0];
 
   return (
     <>
@@ -28,7 +28,8 @@ export default async function ProductsPage() {
           </li>
         ))}
       </ul>
-      <article className={styles.article}>{factText}</article>
+      {/* 클라이언트 컴포넌트를 만들어서 부분적으로 CSR을 할 수 있다. 이것을 하이브리드 개념이라고 한다. */}
+      <MeowArticle />
     </>
   );
 }
