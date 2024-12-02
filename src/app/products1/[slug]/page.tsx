ee9a3ecf99a1b688812fbf006a96addbc919d5f4
pji0219 <pji219@naver.com>
2024-12-02 23:58:20 +0900

    동적 메타 데이터

--- src/app/products1/[slug]/page.tsx
+++ src/app/products1/[slug]/page.tsx
@@ -4,6 +4,23 @@ type Props = {
   };
 };
 
+// 동적 메타데이터
+export function generateMetadata({ params: { slug } }: Props) {
+  return {
+    title: `제품 이름: ${slug}`,
+  };
+}
+
+// 제품 설명 페이지의 기본 골격은 프리 렌더링 됨 params로 받아온 키워드에 대한 페이지는 요청 시에 SSR
 export default function PantPage({ params }: Props) {
   return <h1>{params.slug}제품 설명 페이지</h1>;
+}
+
+// 빌드 시에 해당 키워드에 한해서는 미리 페이지를 만들어 놓음
+// 이렇게 하지 않은 키워드는 요청이 될 때 페이지를 만들고 SSR로 렌더링 됨
+export function generateStaticParams() {
+  const products = ['pants', 'skirt'];
+  return products.map((product) => ({
+    slug: product,
+  }));
 }
