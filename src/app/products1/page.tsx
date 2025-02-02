918ed4c853f286724373da15f5a73145b6557ae6
pji0219 <pji219@naver.com>
2025-02-02 23:42:08 +0900

    1

--- src/app/products1/page.tsx
+++ src/app/products1/page.tsx
@@ -1,5 +1,19 @@
+import Link from 'next/link';
 import React from 'react';
 
+const products = ['pants', 'skirt', 'shirt', 'shoes'];
+
 export default function ProductsPage() {
-  return <h1>제품 페이지</h1>;
+  return (
+    <>
+      <h1>제품 페이지</h1>
+      <ul>
+        {products.map((product, index) => (
+          <li key={index}>
+            <Link href={`/products1/${product}`}>{product}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
