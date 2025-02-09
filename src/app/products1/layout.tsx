667450a670390008cd3005d28d70b5b9f618b25d
pji0219 <pji219@naver.com>
2025-02-09 23:28:08 +0900

    로딩

--- src/app/products1/layout.tsx
+++ src/app/products1/layout.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 export const metadata: Metadata = {
   title: '멋진 제품 사이트 | 전체 제품 확인',
-  description: '멋진 제품을 판매하는 곳입니다.',
+  description: '제품을 확인해 보세요.',
   icons: {
     icon: '/favicon.ico',
   },
--- src/app/products1/page.tsx
+++ src/app/products1/page.tsx
@@ -1,19 +1,22 @@
+import MeowArticle from '@/components/MeowArticle';
+import { getProducts } from '@/service/products';
 import Link from 'next/link';
 import React from 'react';
 
-const products = ['pants', 'skirt', 'shirt', 'shoes'];
+export default async function ProductsPage() {
+  const products = await getProducts();
 
-export default function ProductsPage() {
   return (
     <>
       <h1>제품 페이지</h1>
       <ul>
-        {products.map((product, index) => (
+        {products.map(({ id, name }, index) => (
           <li key={index}>
-            <Link href={`/products1/${product}`}>{product}</Link>
+            <Link href={`/products1/${id}`}>{name}</Link>
           </li>
         ))}
       </ul>
+      <MeowArticle />
     </>
   );
 }
